e2153ece142e2950e391fadb47494d1d8b0367cf
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-02-15 19:03:51 +0800

    Delete src/main.tsx

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-// Find the root element in index.html and create a React root
-createRoot(document.getElementById("root")!).render(<App />);
